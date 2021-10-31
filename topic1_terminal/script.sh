229e2560246b9a780bf960458374e7f22f1147cd
dashenkan <dashenka_n@list.ru>
2021-10-31 20:00:47 +0300

    add terminal homework

---
+++ topic1_terminal/script.sh
@@ -0,0 +1,5 @@
+mkdir folder1 folder2 folder3
+touch folder1/text_1.txt folder1/text_2.txt folder1/text_3.txt folder1/json_1.json folder1/json_2.json
+mkdir folder1/folder4 folder1/folder5 folder1/folder6
+ls -la
+mv folder1/text_1.txt folder2/text_1.txt
